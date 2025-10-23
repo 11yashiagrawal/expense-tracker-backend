00821cb135a5d25e11b1c033b33c7bd4c37a5819
11yashiagrawal <11yashiagrawal@gmail.com>
2025-10-23 13:12:12 +0530

    Added login/logout and refresh token endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(upload.single("avatar"), registerUser);
+
+router.route("/login").post(loginUser);
+
+//Secured routes
+
+router.route("/logout").post(verifyJWT, logoutUser);
+
+router.route("/refreshAccessToken").post(refreshAccessToken);
 
 export default router;
